c84d5a856a0afd04a0502c399dcf466021d04b1b
yassermora <129242285+yassermora@users.noreply.github.com>
2023-12-02 19:26:26 -0600

    Update Reportes.java

--- SuperMercado/src/main/java/com/mycompany/supermercado/Reportes.java
+++ SuperMercado/src/main/java/com/mycompany/supermercado/Reportes.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.supermercado;
-/**
- *
- * @author Usuario xan
- */
+
+import java.util.Date;
+import java.util.List;
+
 public class Reportes {
     private SuperMercado SuperMercado;
 
@@ -24,5 +20,26 @@ public class Reportes {
         for (int i = 0; i < SuperMercado.getPasillos().length; i++) {
             SuperMercado.getPasillos()[i].mostrarOrganizacion();
         }
-    }   
+    }
+    
+    public static void generarReporteCaducidad(List<Producto> listaProductos) {
+    Date fechaActual = new Date(); // Obtener la fecha actual
+
+    for (Producto producto : listaProductos) { // Recorrer la lista de productos
+        Date fechaCaducidad = producto.getFechaCaducidad(); // Obtener la fecha de caducidad del producto actual
+
+        // Calcular la diferencia en milisegundos entre la fecha de caducidad y la fecha actual
+        long diffTiempo = fechaCaducidad.getTime() - fechaActual.getTime();
+        long diffDias = diffTiempo / (1000 * 60 * 60 * 24); // Convertir la diferencia a días
+
+        // Verificar si el producto está próximo a vencer (dentro de los próximos 30 días)
+        if (diffDias <= 30 && diffDias >= 0) {
+            // Imprimir un mensaje indicando que el producto está próximo a vencer junto con su nombre y fecha de caducidad
+            System.out.println("Producto próximo a vencer: " + producto.getNombre() + ", Fecha de Caducidad: " + producto.getFechaCaducidad());
+        }
+    }
+}
+
+
+    
 }
