df8b087a0d0eaae75c2080c536ea1e97affb1fed
Chalton <136747173+xan1-1@users.noreply.github.com>
2023-12-05 13:59:09 -0600

    Update Categoria.java

--- SuperMercado/src/main/java/com/mycompany/supermercado/Categoria.java
+++ SuperMercado/src/main/java/com/mycompany/supermercado/Categoria.java
@@ -2,8 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.supermercado;
-
+package com.mycompany.menuprincipal;
 /**
  *
  * @author Usuario xan
