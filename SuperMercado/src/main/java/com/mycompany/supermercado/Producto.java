7bfd7808b4661d20f1ad9eb0c3b2c44c01b68a92
Chalton <136747173+xan1-1@users.noreply.github.com>
2023-12-05 13:56:52 -0600

    Update Producto.java

--- SuperMercado/src/main/java/com/mycompany/supermercado/Producto.java
+++ SuperMercado/src/main/java/com/mycompany/supermercado/Producto.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.supermercado;
+package com.mycompany.menuprincipal;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 /**
  *
- * @author Usuario xan
+ * @author Usuario
  */
 public class Producto {
     private static int nextId = 1;
